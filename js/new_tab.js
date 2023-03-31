b1a02f7f6f4e47a7efa7e21e594c723beb566ada
Wayne <wayne5540.au+fork@gmail.com>
2023-03-30 20:42:40 -0700

    Add loading gif when loading video

--- js/new_tab.js
+++ js/new_tab.js
@@ -1,4 +1,8 @@
-var videosUrl = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json"
+const videosUrl = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json"
+
+const randomFromArray = function(array) {
+  return array[Math.floor(Math.random()*array.length)]
+}
 
 const getRandomVideoUrl = (videoCategories) => {
   let randomVideoCategory = randomFromArray(videoCategories);
@@ -7,20 +11,22 @@ const getRandomVideoUrl = (videoCategories) => {
   return randomVideo.url;
 };
 
-var init = function() {
+const init = function() {
   $.ajax({
     dataType: "json",
     url: videosUrl,
   }).done(function(videoCategories){
-    var randomVideoUrl = getRandomVideoUrl(videoCategories);
+    let randomVideoUrl = getRandomVideoUrl(videoCategories);
+    let video = document.getElementById('backgroundVideo');
+    let source = document.createElement('source');
+    let loadingImage = document.getElementById('loading-gif');
 
-    var video = document.getElementById('backgroundVideo');
-    var source = document.createElement('source');
     source.setAttribute('src', randomVideoUrl);
     video.appendChild(source);
-
     video.addEventListener("loadeddata", () => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/readyState
       if (video.readyState >= 2) {
+        loadingImage.setAttribute('style', "display: none;");
         video.setAttribute('style', "display: inline-block;");
         video.play();
       }
@@ -28,10 +34,4 @@ var init = function() {
   });
 }
 
-
 init();
-
-
-var randomFromArray = function(array) {
-  return array[Math.floor(Math.random()*array.length)]
-}
